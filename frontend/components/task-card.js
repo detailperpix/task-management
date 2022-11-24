ad7eff22d1fecb5df1f814cebb2966666195780c
Wirmantono <wirmantonochong@gmail.com>
2022-11-24 19:54:25 +0700

    Modify elapsed time in task card

--- frontend/components/task-card.js
+++ frontend/components/task-card.js
@@ -1,4 +1,13 @@
-export default function RunningTask({ data }, idx) {
+function humanReadableTime(date) {
+    // assume passed starttime
+    const startTime = new Date(date);
+    const currentTime = Date.now();
+    const hour = Math.round((currentTime - startTime) / 3600000)
+    const mins = Math.round((currentTime - startTime) / 60000)
+    return  (hour) >= 1 ? hour.toString() + " hr" : mins.toString() + " mins"
+
+}
+export default function RunningTask({ data, idx }) {
     return (
         <div class="flex gap-2 p-6 my-1 max-w-3xl mx-auto bg-white rounded-xl shadow-lg items-center space-y-2">
 
@@ -9,7 +18,7 @@ export default function RunningTask({ data }, idx) {
 
             <div>
                 <p class="">{data.startTime}</p>
-                <p>{Math.round((Date.now() - new Date(data.startTime)) / 3600000)}h elapsed</p>
+                {data.endTime ? <p>{data.endTime} </p>: <p> {humanReadableTime(data.startTime)} elapsed</p>}
             </div>
             <button class="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
                 Finish Task
