ad6448d2fb10db646a6a729d31e880a36c64c995
Wirmantono <wirmantonochong@gmail.com>
2022-11-24 17:40:58 +0700

    Remove id argument in mutation

--- backend/taskmanagement/tasks/mutations.py
+++ backend/taskmanagement/tasks/mutations.py
@@ -4,14 +4,13 @@ import graphene
 
 class AddTaskMutation(graphene.Mutation):
     class Arguments:
-        id = graphene.ID()
         name = graphene.String(required=True)
         description = graphene.String(required=True)
 
     task = graphene.Field(TaskType)
 
     @classmethod
-    def mutate(cls, root, info, id, name, description):
+    def mutate(cls, root, info, name, description):
         task = Task(name=name, description=description)
         task.name = name
         task.description = description
